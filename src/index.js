11fd46cb018654030458ade143254ead26e5ef5e
Bruno Sales <baliestri@icloud.com>
2022-03-08 15:52:55 -0300

    add /users route
    add name and username validation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const { v4: uuid } = require('uuid');
 
-// const { v4: uuidv4 } = require('uuid');
+const NAME_PATTERN = /^((\p{Lu}{1})\S(\p{Ll}{1,20})[^0-9])+[-'\s]((\p{Lu}{1})\S(\p{Ll}{1,20}))*[^0-9]$/u
+const USERNAME_PATTERN = /^[a-zA-Z]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$/u
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body
+
+  if (!username || !name)
+    return response.status(400).json({ error: "missing required fields" })
+
+  if (!NAME_PATTERN.test(name))
+    return response.status(400).json({ error: "invalid name" })
+
+  if (!USERNAME_PATTERN.test(username))
+    return response.status(400).json({ error: "invalid username" })
+  
+  const findUsername = users.some((user) => user.username === username)
+
+  if (findUsername)
+    return response.status(400).json({ error: "username already exists" })
+
+  users.push({ id: uuid(), username, name, todos: [] })
+
+  return response.status(201).send()
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
